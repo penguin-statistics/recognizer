9d7255f561743720c8e103b0ba52b6574d3203ae
Kumo <zhanghaoyun0803@gmail.com>
2021-03-04 20:10:04 +0800

    update

--- penguin.hpp
+++ penguin.hpp
@@ -3,12 +3,12 @@
 
 #include "json.hpp"
 // #include <iomanip>
+#include <any>
 #include <iostream>
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/imgproc.hpp>
-#include <set>
 using dict = nlohmann::json;
 
 void show_img(cv::Mat src)
@@ -29,12 +29,52 @@ const int TEMPLATE_WIDTH = 183;
 const int TEMPLATE_HEIGHT = 183;
 const int TEMPLATE_DIAMETER = 163;
 const int ITEM_RESIZED_WIDTH = 50;
+const double _ITEM_QTY_Y_PROP = 0.7;
+const double _ITEM_QTY_WIDTH_PROP = 0.83;
+const double _ITEM_QTY_HEIGHT_PROP = 0.16;
+const double _ITEM_CHR_HEIGHT_PROP = 0.7;
 } // namespace penguin
 
-namespace penguin { // extern variable
+namespace penguin { // global
 std::string server;
-dict item_index, hash_index;
-std::map<std::string, cv::Mat> item_templs;
+
+class Resource {
+public:
+    static Resource& get_instance()
+    {
+        static Resource instance;
+        return instance;
+    }
+    void add(const std::string& name, const std::any& src)
+    {
+        if (!name.empty()) {
+            _resource[name] = src;
+        }
+    }
+    template <typename SrcType>
+    const SrcType& get(const std::string& name)
+    {
+        if (auto it = _resource.find(name); it != _resource.end()) {
+            SrcType& res = std::any_cast<SrcType&>(it->second);
+            return res;
+        } else {
+            SrcType& res = std::any_cast<SrcType&>(_resource[""]);
+            return res;
+        }
+    }
+
+private:
+    std::map<std::string, std::any> _resource;
+    Resource()
+    {
+        _resource[""] = std::any();
+    };
+    Resource(const Resource&) = delete;
+    Resource(Resource&&) = delete;
+    Resource& operator=(const Resource&) = delete;
+    Resource& operator=(Resource&&) = delete;
+};
+auto& resource = Resource::get_instance();
 } // namespace penguin
 
 namespace penguin { // tools function
@@ -53,11 +93,11 @@ std::list<cv::Range> separate(const cv::Mat& src_bin, DirectionFlags direc, int 
 {
     std::list<cv::Range> sp;
     bool isodd = false;
-    uint begin;
+    int begin;
     if (direc == TOP) {
-        for (uint ro = 0; ro < src_bin.rows; ro++) {
+        for (int ro = 0; ro < src_bin.rows; ro++) {
             uchar* pix = src_bin.data + ro * src_bin.step;
-            uint co = 0;
+            int co = 0;
             for (; co < src_bin.cols; co++) {
                 if ((bool)*pix && !isodd) {
                     begin = ro;
@@ -68,21 +108,21 @@ std::list<cv::Range> separate(const cv::Mat& src_bin, DirectionFlags direc, int 
                 pix++;
             }
             if (co == src_bin.cols && isodd) {
-                uint end = ro;
-                sp.push_back(cv::Range(begin, end));
+                int end = ro;
+                sp.emplace_back(cv::Range(begin, end));
                 isodd = !isodd;
                 if (sp.size() == n)
                     break;
             }
         }
         if (isodd) {
-            uint end = src_bin.rows;
-            sp.push_back(cv::Range(begin, end));
+            int end = src_bin.rows;
+            sp.emplace_back(cv::Range(begin, end));
         }
     } else if (direc == BOTTOM) {
-        for (uint ro = src_bin.rows - 1; ro >= 0; ro--) {
+        for (int ro = src_bin.rows - 1; ro >= 0; ro--) {
             uchar* pix = src_bin.data + ro * src_bin.step;
-            uint co = 0;
+            int co = 0;
             for (; co < src_bin.cols; co++) {
                 if ((bool)*pix && !isodd) {
                     begin = ro + 1;
@@ -93,21 +133,21 @@ std::list<cv::Range> separate(const cv::Mat& src_bin, DirectionFlags direc, int 
                 pix++;
             }
             if (co == src_bin.cols && isodd) {
-                uint end = ro + 1;
-                sp.push_back(cv::Range(begin, end));
+                int end = ro + 1;
+                sp.emplace_back(cv::Range(end, begin));
                 isodd = !isodd;
                 if (sp.size() == n)
                     break;
             }
         }
         if (isodd) {
-            uint end = 0;
-            sp.push_back(cv::Range(begin, end));
+            int end = 0;
+            sp.emplace_back(cv::Range(end, begin));
         }
     } else if (direc == LEFT) {
-        for (uint co = 0; co < src_bin.cols; co++) {
+        for (int co = 0; co < src_bin.cols; co++) {
             uchar* pix = src_bin.data + co;
-            uint ro = 0;
+            int ro = 0;
             for (; ro < src_bin.rows; ro++) {
                 if ((bool)*pix && !isodd) {
                     begin = co;
@@ -118,21 +158,21 @@ std::list<cv::Range> separate(const cv::Mat& src_bin, DirectionFlags direc, int 
                 pix = pix + src_bin.step;
             }
             if (ro == src_bin.rows && isodd) {
-                uint end = co;
-                sp.push_back(cv::Range(begin, end));
+                int end = co;
+                sp.emplace_back(cv::Range(begin, end));
                 isodd = !isodd;
                 if (sp.size() == 2 * n)
                     break;
             }
         }
         if (isodd) {
-            uint end = src_bin.cols;
-            sp.push_back(cv::Range(begin, end));
+            int end = src_bin.cols;
+            sp.emplace_back(cv::Range(begin, end));
         }
     } else if (direc == RIGHT) {
-        for (uint co = src_bin.cols - 1; co >= 0; co--) {
+        for (int co = src_bin.cols - 1; co >= 0; co--) {
             uchar* pix = src_bin.data + co;
-            uint ro = 0;
+            int ro = 0;
             for (; ro < src_bin.rows; ro++) {
                 if ((bool)*pix && !isodd) {
                     begin = co + 1;
@@ -143,16 +183,16 @@ std::list<cv::Range> separate(const cv::Mat& src_bin, DirectionFlags direc, int 
                 pix = pix + src_bin.step;
             }
             if (ro == src_bin.rows && isodd) {
-                uint end = co + 1;
-                sp.push_back(cv::Range(begin, end));
+                int end = co + 1;
+                sp.emplace_back(cv::Range(end, begin));
                 isodd = !isodd;
                 if (sp.size() == 2 * n)
                     break;
             }
         }
         if (isodd) {
-            uint end = 0;
-            sp.push_back(cv::Range(begin, end));
+            int end = 0;
+            sp.emplace_back(cv::Range(end, begin));
         }
     }
     return sp;
@@ -243,34 +283,79 @@ enum RectFlags {
     HEIGHT = 3
 };
 
-class Widget : public cv::Rect {
+class Widget {
 public:
-    Widget() { }
-    Widget(const cv::Mat& img, const cv::Point& topleft)
-        : cv::Rect(topleft, img.size())
+    int x = 0;
+    int y = 0;
+    const int& width = _img.cols;
+    const int& height = _img.rows;
+    Widget() = default;
+    Widget(const Widget& widget)
     {
+        _img = widget._img;
+        x = widget.x;
+        y = widget.y;
+        _parent = widget._parent;
+    }
+    Widget(Widget&& widget) noexcept
+    {
+        _img = widget._img;
+        x = widget.x;
+        y = widget.y;
+        _parent = widget._parent;
+    }
+    Widget(const cv::Mat& img, Widget* const widget = nullptr)
+    {
+        _parent = widget;
+        auto& parent = *_parent;
+        auto& self = *this;
         _img = img;
+        if (_parent != nullptr) {
+            _relate(parent);
+        }
     }
-    Widget(const cv::Mat& img, const cv::Rect& rect)
-        : Widget(img, rect.tl())
+    bool empty() const
     {
+        return width <= 0 || height <= 0;
     }
-    void relate(const Widget& widget)
+    void show() const
+    {
+        show_img(_img);
+    }
+    Widget& operator=(Widget& widget)
+    {
+        if (this != &widget) {
+            x = widget.x;
+            y = widget.y;
+            _img = widget._img;
+        }
+        return *this;
+    }
+    Widget& operator=(Widget&& widget) noexcept
+    {
+        if (this != &widget) {
+            x = widget.x;
+            y = widget.y;
+            _img = widget._img;
+        }
+        return *this;
+    }
+
+protected:
+    cv::Mat _img;
+    Widget* _parent = nullptr;
+    void _relate(Widget& widget)
     {
         auto& self = *this;
         self.x += widget.x;
         self.y += widget.y;
     }
-    void relate(const cv::Point& topleft)
+    void _relate(const cv::Point& topleft)
     {
         auto& self = *this;
         self.x += topleft.x;
         self.y += topleft.y;
     }
-
-protected:
-    cv::Mat _img;
-    std::set<std::reference_wrapper<Widget>> child;
 };
 
 enum CharFlags {
@@ -279,11 +364,12 @@ enum CharFlags {
 };
 
 enum FontFlags {
-    NOVECENTO_WIDEBOLD = 0,
-    SOURCE_HAN_SANS_CN_MEDIUM = 1,
-    NUBER_NEXT_DEMIBOLD_CONDENSED = 2,
-    RODIN_PRO_DB = 3,
-    SOURCE_HAN_SANS_KR_BOLD = 4
+    FONT_UNDEFINED = 0,
+    FONT_NOVECENTO_WIDEBOLD = 1,
+    FONT_SOURCE_HAN_SANS_CN_MEDIUM = 2,
+    FONT_NUBER_NEXT_DEMIBOLD_CONDENSED = 3,
+    FONT_RODIN_PRO_DB = 4,
+    FONT_SOURCE_HAN_SANS_KR_BOLD = 5
 };
 
 class Character : public Widget {
@@ -301,38 +387,38 @@ public:
     const std::string chr() const { return _chr; }
     const int dist() const { return _dist; }
     Character() { }
-    Character(const cv::Mat& img_bin, const cv::Point& topleft, FontFlags flag)
-        : Widget(img_bin, topleft)
+    Character(
+        const cv::Mat& img_bin,
+        FontFlags flag,
+        Widget* const parent = nullptr)
+        : Widget(img_bin, parent)
     {
         font = flag;
         _img = _img(cv::boundingRect(_img));
         _get_char();
     }
-    Character(const cv::Mat& img_bin, const cv::Rect& rect, FontFlags flag)
-        : Character(img_bin, rect.tl(), flag)
-    {
-    }
 
 private:
-    std::string _chr;
+    std::string _chr = "";
     int _dist = HAMMING64 * 4;
     FontFlags font;
-    std::string _hash;
+    std::string _hash = "";
     std::vector<CharDist> _dist_list;
     void _get_char()
     {
-        cv::Mat src_bin = _img;
+        auto src_bin = _img;
         squarize(src_bin);
         _hash = shash(src_bin);
         std::string chr;
         dict char_dict;
-        if (font == NOVECENTO_WIDEBOLD)
+        if (const auto& hash_index = resource.get<dict>("hash_index");
+            font == FONT_NOVECENTO_WIDEBOLD)
             char_dict = hash_index["stage"];
         else
             char_dict = hash_index["item"][server];
         for (const auto& [kchar, vhash] : char_dict.items()) {
             int dist = hamming(_hash, vhash, HAMMING64);
-            _dist_list.push_back(CharDist(kchar, dist));
+            _dist_list.emplace_back(CharDist(kchar, dist));
             if (dist < _dist) {
                 _chr = kchar;
                 _dist = dist;
@@ -346,43 +432,103 @@ private:
     }
 };
 
-std::map<std::string, FontFlags> Server2Font {
-    { "CN", SOURCE_HAN_SANS_CN_MEDIUM },
-    { "US", NUBER_NEXT_DEMIBOLD_CONDENSED },
-    { "JP", RODIN_PRO_DB },
-    { "KR", SOURCE_HAN_SANS_KR_BOLD }
-};
-
 class ItemQuantity : public Widget {
+    std::map<std::string, FontFlags> Server2Font {
+        { "CN", FONT_SOURCE_HAN_SANS_CN_MEDIUM },
+        { "US", FONT_NUBER_NEXT_DEMIBOLD_CONDENSED },
+        { "JP", FONT_RODIN_PRO_DB },
+        { "KR", FONT_SOURCE_HAN_SANS_KR_BOLD }
+    };
+
 public:
-    int quantity() { return _quantity; }
+    const int quantity() const { return _quantity; }
     ItemQuantity() { }
-    ItemQuantity(const cv::Mat& img_bin, const cv::Point& topleft)
-        : Widget(img_bin, topleft)
+    ItemQuantity(const cv::Mat& img, Widget* const parent = nullptr)
+        : Widget(img, parent)
     {
+        if (_img.channels() != 1) {
+            cv::cvtColor(_img, _img, cv::COLOR_BGR2GRAY);
+        }
+        cv::threshold(_img, _img, 127, 255, cv::THRESH_BINARY);
         _get_quantity();
     }
-    ItemQuantity(const cv::Mat& img_bin, const cv::Rect& rect)
-        : Widget(img_bin, rect)
+
+    const Character& operator[](uint i) const
     {
+        auto it = _characters.begin();
+        std::advance(it, i);
+        return *it;
     }
-    const Character& operator[](uint i) const { return _quantity_[i]; }
 
 private:
     uint _quantity = 0;
-    std::vector<Character> _quantity_;
+    std::list<Character> _characters;
     void _get_quantity()
     {
+        auto& self = *this;
+        auto qtyimg = _img;
         std::string quantity_str = "";
-        auto sp = separate(_img, LEFT);
-        for (const auto& range : sp) {
-            cv::Mat charimg = _img(cv::Range(0, _img.rows), range);
-            auto chr = Character(charimg, cv::Point(range.start, 0), Server2Font[server]);
-            chr.relate(*this);
-            quantity_str += chr.chr();
-            _quantity_.push_back(chr);
+        auto sp = separate(qtyimg, RIGHT);
+        sp.remove_if([&](const cv::Range& range) {
+            int length = range.end - range.start;
+            return length > height * 0.5;
+        });
+        auto it = sp.cbegin();
+        for (auto& range : sp) {
+            int length = range.end - range.start;
+            bool quantity_empty = quantity_str.empty();
+            cv::Mat charimg = qtyimg(cv::Rect(
+                range.start, 0, length, height));
+
+            cv::Mat _;
+            cv::Mat1i stats;
+            cv::Mat1d centroids;
+            int ccomps = cv::connectedComponentsWithStats(
+                charimg, _, stats, centroids);
+            if (ccomps - 1 != 1) {
+                if (quantity_empty) {
+                    sp.erase(it);
+                    ++it;
+                    continue;
+                } else {
+                    sp.erase(it, sp.cend());
+                    break;
+                }
+            }
+            if (auto ccomp = cv::Rect(
+                    stats(1, cv::CC_STAT_LEFT), stats(1, cv::CC_STAT_TOP),
+                    stats(1, cv::CC_STAT_WIDTH), stats(1, cv::CC_STAT_HEIGHT));
+                ccomp.width > ccomp.height
+                || ccomp.height / (double)height < _ITEM_CHR_HEIGHT_PROP) {
+                if (quantity_empty) {
+                    sp.erase(it);
+                    ++it;
+                    continue;
+                } else {
+                    sp.erase(it, sp.cend());
+                    break;
+                }
+            }
+            auto chr = Character(charimg, Server2Font[server], this);
+            if (auto chr_str = chr.chr(); chr_str == "W") {
+                quantity_str.insert(0, "0000");
+            } else if (chr_str == "K") {
+                quantity_str.insert(0, "000");
+            } else {
+                quantity_str.insert(0, chr_str);
+            }
+            _characters.push_front(chr);
+            ++it;
         }
-        _quantity = std::stoi(quantity_str); //try-catch
+        if (!quantity_str.empty()) {
+            _quantity = std::stoi(quantity_str);
+            cv::Point topleft = cv::Point(sp.back().start, 0);
+            cv::Point bottomright = cv::Point(sp.front().end, height - 1);
+            _img = qtyimg(cv::Rect(topleft, bottomright));
+            self._relate(topleft);
+        } else {
+            _img = cv::Mat();
+        }
     }
 };
 
@@ -399,17 +545,25 @@ class ItemTemplates {
     };
 
 public:
-    std::list<Templ> _templ_list;
+    const std::list<Templ>& templ_list() const
+    {
+        return _templ_list;
+    };
     ItemTemplates()
     {
+        auto item_templs
+            = resource.get<std::map<std::string, cv::Mat>>("item_templs");
         for (auto& templ : item_templs) {
-            _templ_list.push_back(templ);
+            _templ_list.emplace_back(templ);
         }
     }
     ItemTemplates(const Stage& stage)
     {
         // TODO
     }
+
+private:
+    std::list<Templ> _templ_list;
 };
 
 class Item : public Widget {
@@ -424,26 +578,19 @@ class Item : public Widget {
     };
 
 public:
-    const std::string& itemId() { return _itemId; }
+    const std::string& itemId() const { return _itemId; }
+    const int quantity() const { return _quantity.quantity(); }
     Item() { }
     Item(
         const cv::Mat& img,
-        const cv::Point& topleft,
         double diameter,
-        const ItemTemplates& templs = ItemTemplates())
-        : Widget(img, topleft)
+        const ItemTemplates& templs = ItemTemplates(),
+        Widget* const parent = nullptr)
+        : Widget(img, parent)
     {
         _diameter = diameter;
         _get_item(templs);
-        // _get_quantity();
-    }
-    Item(
-        const cv::Mat& img,
-        const cv::Rect& rect,
-        double diameter,
-        const ItemTemplates& templs = ItemTemplates())
-        : Widget(img, rect)
-    {
+        _get_quantity();
     }
 
 private:
@@ -454,18 +601,15 @@ private:
     std::vector<ItemConfidence> _confidence_list;
     void _get_item(const ItemTemplates& templs)
     {
-        auto s = cv::getTickCount();
-
         auto& self = *this;
-        cv::Mat itemimg;
-        double coeff = (double)ITEM_RESIZED_WIDTH / _img.cols;
+        auto itemimg = _img;
+        int coeff_multiinv = width / ITEM_RESIZED_WIDTH;
+        double coeff = 1.0 / coeff_multiinv;
         if (coeff < 1) {
-            int width = ITEM_RESIZED_WIDTH;
-            int height = round(_img.rows * coeff);
-            resize(_img, itemimg, cv::Size(width, height));
+            resize(itemimg, itemimg, cv::Size(), coeff, coeff, cv::INTER_AREA);
         }
         std::map<std::string, cv::Point> _tmp_itemId2loc;
-        for (const auto& templ : templs._templ_list) {
+        for (const auto& templ : templs.templ_list()) {
             const std::string& itemId = templ.itemId;
             cv::Mat templimg = templ.img;
             double fx = _diameter * coeff / TEMPLATE_DIAMETER;
@@ -478,7 +622,7 @@ private:
             double minval, maxval;
             cv::Point minloc, maxloc;
             cv::minMaxLoc(resimg, &minval, &maxval, &minloc, &maxloc);
-            _confidence_list.push_back(ItemConfidence(itemId, maxval));
+            _confidence_list.emplace_back(ItemConfidence(itemId, maxval));
             _tmp_itemId2loc[itemId] = maxloc;
         }
         std::sort(_confidence_list.begin(), _confidence_list.end(),
@@ -487,28 +631,33 @@ private:
             });
         std::string itemId = _confidence_list.front().itemId;
         cv::Point topleft_new = _tmp_itemId2loc[itemId];
+        topleft_new.x *= coeff_multiinv;
+        topleft_new.y *= coeff_multiinv;
         cv::Size size_new = cv::Size(
             round(TEMPLATE_WIDTH * ((double)_diameter / TEMPLATE_DIAMETER)),
             round(TEMPLATE_HEIGHT * ((double)_diameter / TEMPLATE_DIAMETER)));
         _img = _img(cv::Rect(topleft_new, size_new));
-        self.relate(topleft_new);
+        self._relate(topleft_new);
         _confidence = _confidence_list.front().confidence;
         if (_confidence > 0.9) {
             _itemId = itemId;
         }
-        auto e = cv::getTickCount();
-        std::cout << (e - s) / cv::getTickFrequency() * 1000 << std::endl;
     }
     void _get_quantity()
     {
-        cv::Mat quantityimg;
+        cv::Rect quantityrect = cv::Rect(
+            0, round(height * _ITEM_QTY_Y_PROP),
+            round(width * _ITEM_QTY_WIDTH_PROP),
+            round(height * _ITEM_QTY_HEIGHT_PROP));
+        cv::Mat quantityimg = _img(quantityrect);
+        _quantity = ItemQuantity(quantityimg, this);
     }
 };
 
 namespace result {
-    extern dict stage_index;
+
 } // namespace result
 
 } // namespace penguin
 
-#endif
+#endif // PENGUIN_HPP_
