dd98bccefcc961ef7f2518aac30ffc774c270359
Kumo <zhanghaoyun0803@gmail.com>
2022-02-09 21:26:01 +0800

    fix: select overloaded function

--- src/recognizer_wasm_new.cpp
+++ src/recognizer_wasm_new.cpp
@@ -1,6 +1,9 @@
-#include "recognizer.hpp"
+#include <string>
+
 #include <emscripten.h>
 #include <emscripten/bind.h>
+
+#include "recognizer.hpp"
 
 using namespace emscripten;
 
@@ -16,9 +19,9 @@ EMSCRIPTEN_BINDINGS(recognizer)
     constant("version", version);
     constant("opencv_version", opencv_version);
     function("load_server", &load_server);
-    function("load_stage_index", &load_stage_index);
-    function("load_hash_index", &load_hash_index);
-    function("load_templ", &load_templ);
+    function("load_stage_index", select_overload<void(std::string)>(&load_stage_index));
+    function("load_hash_index", select_overload<void(std::string)>(&load_hash_index));
+    function("load_templs", select_overload<void(std::string, std::string)>(&load_templs));
     function("env_check", &env_check);
     class_<Recognizer>("Recognizer")
         .constructor<std::string>()
