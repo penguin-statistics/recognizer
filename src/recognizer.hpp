9489bbd1e8c7678199dca97a049582ea70699200
Kumo <zhanghaoyun0803@gmail.com>
2022-02-13 03:54:25 +0800

    feat: add debug image getter

--- src/recognizer.hpp
+++ src/recognizer.hpp
@@ -18,13 +18,6 @@
 static const std::string version = "3.4.0";
 static const std::string opencv_version = CV_VERSION;
 
-enum class RecognizerMode
-{
-    UNDEFINED = 0,
-    RESULT = 1,
-    DEPOT = 2
-};
-
 cv::Mat decode(std::string JSarrayBuffer)
 {
     std::vector<uint8_t> buf(JSarrayBuffer.cbegin(), JSarrayBuffer.cend());
@@ -48,11 +41,10 @@ void load_stage_index() // local
     dict stage_index;
     f.open("../resources/json/stage_index.json");
     f >> stage_index;
-    f.close();
     penguin::resource.add("stage_index", stage_index);
 }
 
-void load_stage_index(std::string stage_index) // wasm
+void wload_stage_index(std::string stage_index) // wasm
 {
     auto& resource = penguin::resource;
     resource.add("stage_index", dict::parse(stage_index));
@@ -64,11 +56,10 @@ void load_hash_index() // local
     dict hash_index;
     f.open("../resources/json/hash_index.json");
     f >> hash_index;
-    f.close();
     penguin::resource.add("hash_index", hash_index);
 }
 
-void load_hash_index(std::string hash_index) // wasm
+void wload_hash_index(std::string hash_index) // wasm
 {
     auto& resource = penguin::resource;
     resource.add("hash_index", dict::parse(hash_index));
@@ -87,7 +78,7 @@ void load_templs() // local
     penguin::resource.add("item_templs", item_templs);
 }
 
-void load_templs(std::string itemId, std::string JSarrayBuffer) // wasm
+void wload_templs(std::string itemId, std::string JSarrayBuffer) // wasm
 {
     cv::Mat templimg = decode(JSarrayBuffer);
     auto& resource = penguin::resource;
@@ -108,72 +99,117 @@ const bool env_check()
 class Recognizer
 {
 public:
-    Recognizer(RecognizerMode mode)
+    Recognizer(std::string mode)
     {
         _mode = mode;
     }
-    Recognizer(std::string mode)
-    {
-        if (mode == "RESULT")
-        {
-            _mode = RecognizerMode::RESULT;
-        }
-        else if (mode == "DEPOT")
-        {
-            _mode = RecognizerMode::DEPOT;
-        }
-        else
-        {
-            _mode = RecognizerMode::UNDEFINED;
-        }
-    }
 
-    void set_image(cv::Mat img)
+    void set_img(cv::Mat img) // local
     {
         _img = img;
     }
-    void set_image(std::string JSarrayBuffer)
+    void wset_img(std::string JSarrayBuffer) // wasm
     {
         int64 start = cv::getTickCount();
         _img = decode(JSarrayBuffer);
         int64 end = cv::getTickCount();
-        _report["cost"]["decode"] =
-            std::to_string((end - start) / cv::getTickFrequency() * 1000) + "ms";
-    }
-
-    std::string get_report()
-    {
-        return _report.dump();
+        _decode_time = (end - start) / cv::getTickFrequency() * 1000;
     }
 
     void recognize()
     {
-        switch (_mode)
-        {
-        case RecognizerMode::RESULT:
+        if (_mode == "RESULT")
         {
             int64 start = cv::getTickCount();
-            penguin::Result result {_img};
-            result.analyze();
+            _result.set_img(_img);
+            _result.analyze();
             int64 end = cv::getTickCount();
-            _report.merge_patch(result.report());
-            _report["md5"] = result.get_md5();
-            _report["cost"]["recognize"] =
-                std::to_string((end - start) / cv::getTickFrequency() * 1000) + "ms";
-            break;
+            _md5 = _result.get_md5();
+            _recognize_time = (end - start) / cv::getTickFrequency() * 1000;
         }
-        default:
+    }
+    void wrecognize()
+    {
+        recognize();
+    }
+
+    dict get_report(bool detail = false)
+    {
+        dict report;
+        if (_mode == "RESULT")
         {
-            _report = "[ERROR] Invalid Recognizer Mode";
-            break;
+            if (detail)
+            {
+                report.merge_patch(_result.report(true));
+            }
+            else
+            {
+                report.merge_patch(_result.report());
+            }
+            report["md5"] = _md5;
+            if (const auto& decode_time = _decode_time; decode_time)
+            {
+                report["cost"]["decode"] = decode_time;
+            }
+            report["cost"]["recognize"] = _recognize_time;
         }
+        return report;
+    }
+    std::string wget_report(bool detail = false, bool pretty_print = false)
+    {
+        if (pretty_print)
+        {
+            return get_report(detail).dump(4);
+        }
+        else
+        {
+            return get_report(detail).dump();
         }
     }
 
+    cv::Mat get_debug_img()
+    {
+        auto get_rect = [](dict arr) {
+            return cv::Rect(arr[0], arr[1], arr[2], arr[3]);
+        };
+        cv::Mat img = _img.clone();
+        if (const auto& report = get_report();
+            report.contains("exceptions"))
+        {
+            for (const auto& exp : report["exceptions"])
+            {
+                if (exp["type"] == "ERROR" &&
+                    exp["detail"]["rect"] != "empty")
+                {
+                    cv::Rect rect = get_rect(exp["detail"]["rect"]);
+                    cv::rectangle(img, rect, cv::Scalar(0, 0, 255));
+                }
+            }
+        }
+        return img;
+    }
+#ifdef PENGUIN_RECOGNIZER_WASM_CPP_
+#include <emscripten.h>
+#include <emscripten/val.h>
+    emscripten::val wget_debug_img()
+    {
+        auto& img = get_debug_img();
+        std::string buf;
+        cv::imencode("png", img, buf);
+        return emscripten::val(
+            emscripten::typed_memory_view(buf.size(), buf.data()));
+    }
+#endif // PENGUIN_RECOGNIZER_WASM_CPP_
+
 private:
-    RecognizerMode _mode;
+    std::string _mode;
     cv::Mat _img;
+    penguin::Result _result;
+    penguin::Depot _depot;
     dict _report;
+    std::string _md5;
+    double _decode_time = 0;
+    double _recognize_time = 0;
 };
 
 #endif // PENGUIN_RECOGNIZER_HPP_
--- src/recognizer_wasm_new.cpp
+++ src/recognizer_wasm_new.cpp
@@ -1,3 +1,6 @@
+#ifndef PENGUIN_RECOGNIZER_WASM_CPP_
+#define PENGUIN_RECOGNIZER_WASM_CPP_
+
 #include <string>
 
 #include <emscripten.h>
@@ -19,14 +22,16 @@ EMSCRIPTEN_BINDINGS(recognizer)
     constant("version", version);
     constant("opencv_version", opencv_version);
     function("load_server", &load_server);
-    function("load_stage_index", select_overload<void(std::string)>(&load_stage_index));
-    function("load_hash_index", select_overload<void(std::string)>(&load_hash_index));
-    function("load_templs", select_overload<void(std::string, std::string)>(&load_templs));
+    function("load_stage_index", &wload_stage_index);
+    function("load_hash_index", &wload_hash_index);
+    function("load_templs", &wload_templs);
     function("env_check", &env_check);
     class_<Recognizer>("Recognizer")
         .constructor<std::string>()
-        .function("set_image", select_overload<void(std::string)>(&Recognizer::set_image))
-        .function("recognize", &Recognizer::recognize)
-        .function("get_report", &Recognizer::get_report);
+        .function("set_img", &Recognizer::wset_img)
+        .function("recognize", &Recognizer::wrecognize)
+        .function("get_report", &Recognizer::wget_report)
+        .function("get_debug_img", &Recognizer::wget_debug_img);
 }
 #endif // __EMSCRIPTEN__
+#endif // PENGUIN_RECOGNIZER_WASM_CPP_
